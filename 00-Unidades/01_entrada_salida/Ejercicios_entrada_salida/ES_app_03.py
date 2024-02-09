83f95c602dff87761276c93f6c74680a284e5bb4
naza <nazaomfg@gmail.com>
2024-02-09 00:41:10 -0300

    ejercicio E/S03

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_03.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_03.py
@@ -34,7 +34,8 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        nombre = self.txt_nombre.get()
+        alert("informacion", nombre)
                 
     
 if __name__ == "__main__":
